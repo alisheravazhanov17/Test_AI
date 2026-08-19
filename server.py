c4fd9e988adb57679c3f26db63e83104dbdc174d
alisheravazhanov17 <137412809+alisheravazhanov17@users.noreply.github.com>
2026-08-19 12:03:39 +0600

    server.py

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def generate_tests():
                     "content": topic,
                 }
             ],
-            model="llama-3.1-8b-instant",
+            model="llama-3.3-70b-versatile",
             response_format={"type": "json_object"}
         )
         
